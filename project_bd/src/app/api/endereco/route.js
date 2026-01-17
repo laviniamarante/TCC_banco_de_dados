3e312639e16bbd3a42e263c91017ccddea9a288b
laviniamarante <laviinia.amarante@gmail.com>
2026-01-17 16:47:38 -0300

    Refactor POST function to include id_pessoa in validation and database insertion; enhance GET function to retrieve additional fields from pessoa table

--- project_bd/src/app/api/endereco/route.js
+++ project_bd/src/app/api/endereco/route.js
@@ -6,10 +6,16 @@ export async function POST(requisicao) {
     const {
       descricao_logradouro,
       abreviacao_logradouro,
-      id_municipio
+      id_municipio,
+      id_pessoa
     } = await requisicao.json();
 
-    if (!descricao_logradouro || !abreviacao_logradouro || !id_municipio) {
+    if (
+      !descricao_logradouro ||
+      !abreviacao_logradouro ||
+      !id_municipio ||
+      !id_pessoa
+    ) {
       return NextResponse.json(
         { error: "Todos os campos são obrigatórios." },
         { status: 400 }
@@ -18,9 +24,14 @@ export async function POST(requisicao) {
 
     await pool.query(
       `INSERT INTO endereco 
-       (descricao_logradouro, abreviacao_logradouro, id_municipio)
-       VALUES ($1, $2, $3)`,
-      [descricao_logradouro, abreviacao_logradouro, id_municipio]
+       (descricao_logradouro, abreviacao_logradouro, id_municipio, id_pessoa)
+       VALUES ($1, $2, $3, $4)`,
+      [
+        descricao_logradouro,
+        abreviacao_logradouro,
+        id_municipio,
+        id_pessoa
+      ]
     );
 
     return NextResponse.json(
@@ -28,16 +39,37 @@ export async function POST(requisicao) {
       { status: 201 }
     );
   } catch (error) {
-    return NextResponse.json({ error: error.message }, { status: 500 });
+    return NextResponse.json(
+      { error: error.message },
+      { status: 500 }
+    );
   }
 }
+
 export async function GET() {
-    try {
-        const result = await pool.query(`
-            SELECT * FROM endereco ORDER BY id_endereco ASC;`);
-        return NextResponse.json(result.rows, { status: 200 });
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ error: 'Internal Server Error' }, { status: 500 });
-    }
+  try {
+    const result = await pool.query(`
+      SELECT 
+        e.id_endereco,
+        e.descricao_logradouro,
+        e.abreviacao_logradouro,
+        e.id_municipio,
+        e.id_pessoa,
+        p.cnpj_cpf,
+        p.email,
+        p.role
+      FROM endereco e
+      LEFT JOIN pessoa p
+        ON e.id_pessoa = p.id_pessoa
+      ORDER BY e.id_endereco ASC;
+    `);
+
+    return NextResponse.json(result.rows, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
